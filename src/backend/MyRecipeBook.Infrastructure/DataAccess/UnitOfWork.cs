303ee6157c7de7342a4fc8c56ce90ff344deb4a2
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-12-04 14:59:30 -0300

    feat: adding unit of work

---
+++ src/backend/MyRecipeBook.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using MyRecipeBook.Domain.Repositories;
+
+namespace MyRecipeBook.Infrastructure.DataAccess
+{
+    public class UnitOfWork(MyRecipeBookDbContext dbContext) : IUnitOfWork
+    {
+        private readonly MyRecipeBookDbContext _dbContext = dbContext;
+
+        public async Task Commit() => await _dbContext.SaveChangesAsync();
+    }
+}
