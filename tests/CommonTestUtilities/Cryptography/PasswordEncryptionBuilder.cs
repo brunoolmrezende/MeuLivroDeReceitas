4131dcef1a083f18dc8c1713e717dbe18365a68b
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-12-13 14:25:45 -0300

    feat: adding builders for common test utilities

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncryptionBuilder.cs
@@ -0,0 +1,9 @@
+﻿using MyRecipeBook.Application.Services.Cryptography;
+
+namespace CommonTestUtilities.Cryptography
+{
+    public class PasswordEncryptionBuilder
+    {
+        public static PasswordEncryption Build() => new PasswordEncryption("abcd1234");
+    }
+}
---
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class UserReadOnlyRepositoryBuilder
+    {
+        private readonly Mock<IUserReadOnlyRepository> _readOnlyRepository;
+        public UserReadOnlyRepositoryBuilder() => _readOnlyRepository = new Mock<IUserReadOnlyRepository>();
+        public void ExistActiveUserWithEmail(string email)
+        {
+            _readOnlyRepository.Setup(repository => repository.ExistActiveUserWithEmail(email)).ReturnsAsync(true);
+        }
+
+        public IUserReadOnlyRepository Build() => _readOnlyRepository.Object;
+    }
+}
