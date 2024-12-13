58c142729a344733d82f7944623ad191c8e6e82b
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-12-13 14:28:08 -0300

    feat:: adding RegisterUseCaseTest

---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,68 @@
+﻿using CommonTestUtilities.AutoMapper;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionBase;
+
+namespace UseCases.Test.User.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var useCase = CreateUseCase();
+
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(request.Name);
+        }
+
+        [Fact]
+        public async Task Error_Email_Already_Registered()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var useCase = CreateUseCase(request.Email);
+
+            Func<Task> act = async () =>  await useCase.Execute(request);
+
+            await act.Should().ThrowAsync<ErrorOnValidationException>()
+                .Where(error => error.ErrorMessages.Count == 1 && error.ErrorMessages.Contains(ResourceMessagesException.EMAIL_ALREADY_REGISTERED));
+        }
+
+        [Fact]
+        public async Task Error_Name_Empty()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var useCase = CreateUseCase();
+
+            Func<Task> act = async () => await useCase.Execute(request);
+
+            await act.Should().ThrowAsync<ErrorOnValidationException>()
+                .Where(error => error.ErrorMessages.Count == 1 && error.ErrorMessages.Contains(ResourceMessagesException.NAME_EMPTY));
+        }
+        private RegisterUserUseCase CreateUseCase(string? email = null)
+        {
+            var mapper = MapperBuilder.Build();
+            var passwordEncryption = PasswordEncryptionBuilder.Build();
+            var writeOnlyRepository = UserWriteOnlyRepositoryBuilder.Build();
+            var readOnlyRepositoryBuilder = new UserReadOnlyRepositoryBuilder();
+            var unitOfWork = UnitOfWorkBuilder.Build();
+
+            if (string.IsNullOrEmpty(email) == false)
+                readOnlyRepositoryBuilder.ExistActiveUserWithEmail(email);
+
+            return new RegisterUserUseCase(readOnlyRepositoryBuilder.Build(), writeOnlyRepository, mapper, unitOfWork, passwordEncryption);
+        }
+
+    }
+}
