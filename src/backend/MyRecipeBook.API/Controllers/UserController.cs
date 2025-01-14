4353a3a6526bbf82bd93c90f463a6577de6a5e73
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-01-13 23:03:52 -0300

    feat: implement user profile retrieval during authentication

--- src/backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Attributes;
+using MyRecipeBook.Application.UseCases.User.Profile;
 using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
@@ -17,6 +19,16 @@ namespace MyRecipeBook.API.Controllers
             var response = await useCase.Execute(request);
 
             return Created(string.Empty, response);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
+        [AuthenticatedUser]
+        public async Task<IActionResult> GetUserProfile([FromServices] IGetUserProfileUseCase useCase)
+        {
+            var result = await useCase.Execute();
+
+            return Ok(result);
         }
             
     }
--- src/backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
+++ src/backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
 using MyRecipeBook.Domain.Entities;
 
 namespace MyRecipeBook.Application.Services.AutoMapper
@@ -9,12 +10,18 @@ namespace MyRecipeBook.Application.Services.AutoMapper
         public AutoMapping()
         {
             RequestToDomain();
+            DomainToResponse();
         }
 
         private void RequestToDomain()
         {
             CreateMap<RequestRegisterUserJson, User>()
                 .ForMember(dest => dest.Password, opt => opt.Ignore());
+        }
+
+        private void DomainToResponse()
+        {
+            CreateMap<User, ResponseUserProfileJson>();
         }
     }
 }
