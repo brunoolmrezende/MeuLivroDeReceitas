4e4e10c90d2e4c97b1d62718a7afeb2943ea96dd
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-01-17 22:20:26 -0300

    feat: New Migration for Recipe Entity

---
+++ src/backend/MyRecipeBook.Domain/Entities/DishType.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Domain.Entities
+{
+    public class DishType : EntityBase
+    {
+        public Enums.DishType Type { get; set; }
+        public long RecipeId { get; set; } 
+    }
+}
