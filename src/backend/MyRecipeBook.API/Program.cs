cd25458cc62eddd6202a5023f654a5bb5b02e140
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-03-30 16:38:10 -0300

    feat: add Google authentication.

--- src/backend/MyRecipeBook.API/Program.cs
+++ src/backend/MyRecipeBook.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.OpenApi.Models;
 using MyRecipeBook.API.BackgroundServices;
 using MyRecipeBook.API.Converters;
@@ -74,6 +75,8 @@ if (!builder.Configuration.IsUnitTestEnviroment())
     builder.Services.AddHostedService<DeleteUserService>();
 }
 
+AddGoogleAuthentication();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -105,6 +108,22 @@ void MigrateDatabase()
     var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
     
     DatabaseMigration.Migrate(connectionString, serviceScope.ServiceProvider);
+}
+
+void AddGoogleAuthentication()
+{
+    var clientId = builder.Configuration.GetValue<string>("Settings:Google:ClientId")!;
+    var clientSecret = builder.Configuration.GetValue<string>("Settings:Google:ClientSecret")!;
+
+    builder.Services.AddAuthentication(config =>
+    {
+        config.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    }).AddCookie()
+    .AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = clientId;
+        googleOptions.ClientSecret = clientSecret;
+    });
 }
 
 public partial class Program
