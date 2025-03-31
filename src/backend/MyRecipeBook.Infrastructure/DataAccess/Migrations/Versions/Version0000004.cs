2438c6ff7ca2cc87241bad3c6021e5a95aa4d864
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-03-31 18:24:31 -0300

    feat: init migration to add refresh token table.

---
+++ src/backend/MyRecipeBook.Infrastructure/DataAccess/Migrations/Versions/Version0000004.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Migrations.Versions
+{
+    [Migration(DatabaseVersion.TABLE_REFRESH_TOKENS, "Create table to save refresh tokens.")]
+    public class Version0000004 : VersionBase
+    {
+        public override void Up()
+        {
+            CreateTable("RefreshTokens")
+                .WithColumn("Value").AsString().NotNullable()
+                .WithColumn("UserId").AsInt64().NotNullable().ForeignKey("FK_RefreshTokens_User_Id", "Users", "Id");
+        }
+    }
+}
